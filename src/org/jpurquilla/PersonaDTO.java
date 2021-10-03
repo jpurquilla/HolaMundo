4e4fd4276236041efa59ae3b8c7e5780f944f494
jpurquilla <jpurquilla>
2021-10-02 21:15:53 -0600

    Se creo PersonaDTO en main

---
+++ src/org/jpurquilla/PersonaDTO.java
@@ -0,0 +1,26 @@
+package org.jpurquilla;
+
+public class PersonaDTO {
+    private String nombre;
+    private String apellidos;
+
+    public PersonaDTO() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
+
